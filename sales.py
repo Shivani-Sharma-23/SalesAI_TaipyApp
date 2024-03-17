9af6b30382e04aec2930fda9a4d9225bfd715310
Shivani-Sharma-23 <shivanisharma232005@gmail.com>
2024-03-17 19:15:18 +0530

    Done 👍👍

--- sales.py
+++ sales.py
@@ -5,9 +5,9 @@ import sys
 from web_pages.calc.calc import calc_ui
 from web_pages.dashboard.dashboard import dashboard_ui
 from web_pages.intro.intro import intro_ui
-from web_pages.bot.bot import bot_ui
+from SalesAI_TaipyApp.web_pages.bot.bot import bot_ui
 from web_pages.calc.calc import *
-from web_pages.bot.bot import *
+from SalesAI_TaipyApp.web_pages.bot.bot import *
 pages = {
     "/":" <|toggle|theme|> <|navbar|>",
     "AboutUs":intro_ui,
@@ -24,3 +24,6 @@ if __name__ == "__main__":
         image = ("calculator1.png", "Chatbot.png", "Dashboard.png","salescycle.png"),
         use_reloader=True
     )
+
+
+    
